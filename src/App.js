bd9bb895d0df40009a329228ead983cbf201ae07
PaidoNunu <aluizprofi@gmail.com>
2024-03-22 21:42:45 -0300

    Header Completo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Carrosel />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,14 @@ import styles from './Header.module.css'
 import myImage from './logo.jpg'
 function header(){
     return(
+        
         <div className={styles.headerBase}>
-            <div>
-            <div className={styles.imagemLogo}><img src={myImage} alt='Minha imagem'/></div> <h1 >Home</h1><h1 >Conscientizar</h1><h1 >Feira</h1>
-           
-            </div>
-           
-        </div>
+
+           <div className={styles.logo}> <img  src={myImage} alt='Minha imagem'/> </div>
+            <button className={styles.button}>  <a href="https://www.example.com" className={styles.link}>Home</a> </button>
+            <button className={styles.button} ><a href="https://www.example.com"className={styles.link}>Feira</a></button>
+            <button className={styles.button}> <a href="https://www.example.com"className={styles.link}>Conscientize-se</a></button>            
+        </div>  
     )
 }
 export default header
