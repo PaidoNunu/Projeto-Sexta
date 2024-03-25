eca3f226c0341d5f9e313b80cebcf2eed1355e0a
PaidoNunu <aluizprofi@gmail.com>
2024-03-24 22:23:29 -0300

    carrosel inserido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header.js'
+import Carrosel from './components/Carrosel.js'
 import './App.css';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Carrosel />
+      <Carrosel/>
+     
     </div>
   );
 }
---
+++ src/components/Carrosel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Carrosel.module.css'
+function carrosel(){
+    return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>Sobre o programa!</h1>
+        
+
+    </div>
+    )
+}
+export default carrosel
