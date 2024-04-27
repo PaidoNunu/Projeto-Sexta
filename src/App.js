6a45db9cdce2579497af5f86dc9fba08b89e4761
PaidoNunu <aluizprofi@gmail.com>
2024-04-26 22:25:33 -0300

    AEAE

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import styles from './App.css'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
+import Sobre from './Pages/Sobre'
 import logo from './imgs/logo.jpg'
 import PontosDescartes from './Pages/PontosDescartes'
 
+
+const cores = ["Home", "Pontos de", "azul"];
 function Navbar() {
   return (
     <nav className='header'>
@@ -12,6 +15,7 @@ function Navbar() {
       <div className='botaodiv'>
         <button className='botao'><Link to="/" className='link'>Home</Link></button>
         <button className='botao'><Link to="/descartes" className='link'>Pontos de Descartes</Link></button>
+        <button className='botao'><Link to="/sobre" className='link'>Sobre</Link></button>
       </div>
       <button className='entrar'><Link to="/SignUp" className='link_login'>Entrar</Link></button>
     </nav>
@@ -25,6 +29,7 @@ function App() {
         <Route exact path='/' element={<><Navbar/><Home/></>}/>
         <Route path='/SignUp' element={<Login/>}/>
         <Route path='/descartes' element={<><Navbar/><PontosDescartes/></>}/>
+        <Route path='/Sobre' element={<><Navbar/><Sobre/></>}/>
       </Routes>
     </Router>
   );
