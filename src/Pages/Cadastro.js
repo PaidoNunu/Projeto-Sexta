576920722a7178b9851cc790d7a204e1d64d10c6
Bruno Klisman <83792250+Bruno-klsiman@users.noreply.github.com>
2024-05-24 00:14:44 -0300

    Atualização

--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -1,41 +1,44 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import styles from '../components/Cadastro.module.css'
 import { Link } from 'react-router-dom';
-import styles from '../components/Login.module.css'
 import logo from '../imgs/lixozero2.png'
-import bg from '../imgs/background.jpg'
-
-function Login(){
-    let navigate = useNavigate();
-    function voltaHome() {
-        navigate('/');
-    }
-
+function Cadastro(){
     return(
-        <div className={styles.loginpage}>
+        <div className={styles.divmae}>
              <div className={styles.centralizar}>
              <div className={styles.divform}>
-            <img className={styles.imglogin} src={logo} alt=""></img>
+            <img className={styles.imgCadastro} src={logo} alt=""></img>
         <form className={styles.form}>
+        <div className={styles.inputConteiner}>
+                <label className={styles.label} htmlFor="email">Nome</label>
+                <input className={styles.input1} type="text" name="email" id="nome" placeholder="Digite seu nome">
+                </input>
+            </div>
             <div className={styles.inputConteiner}>
                 <label className={styles.label} htmlFor="email">E-mail</label>
                 <input className={styles.input1} type="text" name="email" id="email" placeholder="exemplo@gmail.com">
                 </input>
             </div>
-
             <div className={styles.inputConteiner}>
                 <label className={styles.label} htmlFor="password">Senha</label>
                 <input className={styles.input2} type="password" name="password" id="password" placeholder="*******">
                 </input>
             </div>
+            <div className={styles.inputConteiner}>
+                <label className={styles.label} htmlFor="password">Confirme a senha</label>
+                <input className={styles.input2} type="password" name="password" id="comfirmassword" placeholder="*******">
+                </input>
+            </div>
             <div className={styles.inputbotao}>
-                <input className={styles.input3} type="submit" name="submit" id="submit" value="ENTRAR" onClick={voltaHome}></input>
+                <button className={styles.button} >Cadastro</button>
             </div>
             <Link to="/Cadastro" className={styles.linkCadastro}>Cadastre aqui</Link>
         </form>
         </div>
              </div>
         </div>
-       
-    );
-}export default Login;
+    )
+}
+
+export default Cadastro;
--- src/Pages/Sobre.js
+++ src/Pages/Sobre.js
@@ -8,12 +8,12 @@ function Sobre() {
         <div className={styles.escrito}>
         <h1 className={styles.h1Sobre}>Um compromisso com a sustentabilidade e o futuro do nosso planeta.</h1>
       <p className={styles.pSobre}>
-LIXO ZERO é uma empresa dedicada à reciclagem e ao manejo responsável de resíduos. Acreditamos que a reciclagem é essencial para a preservação do meio ambiente e para a construção de um futuro mais sustentável para as próximas gerações.
+      LIXO ZERO é uma empresa dedicada à reciclagem e ao manejo responsável de resíduos. Acreditamos que a reciclagem é essencial para a preservação do meio ambiente e para a construção de um futuro mais sustentável para as próximas gerações.
       </p>
       <p className={styles.pSobre}>Promover a reciclagem: Incentivamos e facilitamos a reciclagem de diversos materiais, como papel, plástico, metal, vidro e eletrônicos, em nossa comunidade.<br/>
-Reduzir o impacto ambiental: Através da reciclagem, desviamos o lixo dos aterros sanitários, diminuímos a necessidade de extração de recursos naturais e contribuímos para a mitigação das mudanças climáticas.<br/>
-Educar e conscientizar: Acreditamos que a educação é fundamental para promover a cultura da reciclagem. Por isso, oferecemos workshops, palestras e materiais informativos para conscientizar a população sobre a importância da reciclagem e do consumo consciente.<br/>
-Promover a economia circular: Buscamos integrar a reciclagem em um ciclo virtuoso, onde os materiais reciclados são utilizados para a produção de novos produtos, reduzindo o consumo de recursos virgens e promovendo a sustentabilidade da cadeia produtiva.<br/></p>
+      Reduzir o impacto ambiental: Através da reciclagem, desviamos o lixo dos aterros sanitários, diminuímos a necessidade de extração de recursos naturais e contribuímos para a mitigação das mudanças climáticas.<br/>
+      Educar e conscientizar: Acreditamos que a educação é fundamental para promover a cultura da reciclagem. Por isso, oferecemos workshops, palestras e materiais informativos para conscientizar a população sobre a importância da reciclagem e do consumo consciente.<br/>
+      Promover a economia circular: Buscamos integrar a reciclagem em um ciclo virtuoso, onde os materiais reciclados são utilizados para a produção de novos produtos, reduzindo o consumo de recursos virgens e promovendo a sustentabilidade da cadeia produtiva.<br/></p>
         </div>
       
     
